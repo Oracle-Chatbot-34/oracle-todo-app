ae080987d6856c9eb0ed8cf59d15ba3ccc56f90e
DanielBrMz <a01254805@tec.mx>
2025-02-26 12:22:18 -0600

    FE in POM

--- MtdrSpring/backend/undeploy.sh
+++ MtdrSpring/backend/undeploy.sh
@@ -1,3 +1,5 @@
 echo delete frontend deployment and service...
 kubectl -n mtdrworkshop delete deployment todolistapp-springboot-deployment
 kubectl -n mtdrworkshop delete service todolistapp-springboot-service
+
+# Pending to delete
