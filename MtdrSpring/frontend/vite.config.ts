344beb6a4abe250636a4009322d1c8427e98968b
DanielBrMz <a01254805@tec.mx>
2025-02-26 12:22:18 -0600

    FE proxy

--- MtdrSpring/frontend/vite.config.ts
+++ MtdrSpring/frontend/vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-});
+  server: {
+    proxy: {
+      '/todolist': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+      '/auth': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      }
+    }
+  }
+})
